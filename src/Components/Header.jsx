376d68f9c4b35217918496d0f10540054a31cbc6
Robert Horngren <robert@horngren.net>
2021-05-11 17:13:46 +0200

    added content to buttons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,5 @@
+import ProfileIcon from '../Assets/Images/profileIcon.png';
+
 const Header = () => {
 
     const style = {
@@ -11,27 +13,60 @@ const Header = () => {
             background: "#C4C4C4"
         },
         menuButton: {
+            display: "flex",
+            flexDirection: "column",
             marginLeft: 25,
-            background: "red"
         },
         profileButton: {
             marginRight: 25,
-            background: "blue"
+        },
+        menuContainer: {
+            display: "flex",
+            flexDirection: "column",
+            height: 50,
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: -5
+        },
+        menuRow: {
+            height: 7,
+            width: 50,
+            marginTop: 5,
+            background: "black"
+        },
+        buttonImage: {
+            color: "black",
+            width: 40,
+            height: 40
         },
         buttonGeneral: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
             width: 60,
             height: 60,
             borderRadius: 15,
-            border: "1px solid black"
+            border: "1px solid black",
+            background: "white"
         }
     }
 
 
     return (
         <header style={style.header}>
-            <div style={{...style.menuButton, ...style.buttonGeneral}}></div>
+            <div style={{ ...style.menuButton, ...style.buttonGeneral }}>
+                <div style={style.menuContainer}>
+                    <div style={style.menuRow} />
+                    <div style={style.menuRow} />
+                    <div style={style.menuRow} />
+                </div>
+            </div>
+
             <h2>WHS</h2>
-            <div style={{...style.profileButton, ...style.buttonGeneral}}></div>
+            
+            <div style={{ ...style.profileButton, ...style.buttonGeneral }}>
+                <img style={style.buttonImage} src={ProfileIcon} />
+            </div>
         </header>
     )
 }
