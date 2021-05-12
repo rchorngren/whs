406f9e8a82bde8a7dcac4b05f85b6a71a9f2e445
Erizium <tobias.osterlin@gmail.com>
2021-05-12 15:26:30 +0200

    Finished sidebar design

--- src/Components/GenreSidebar/GenreMenu.jsx
+++ src/Components/GenreSidebar/GenreMenu.jsx
@@ -1,20 +1,25 @@
 import React from 'react-redux';
-//import genre array
 
 const GenreMenu = () => {
 
    let placeholder = ['Action','Schi-fi','Thriller']
 
+   const test = (item) => {
+    console.log(item)
+    }
+
+   //creates a div for each item in array
    const genreList = placeholder.map((item) => (
-        <div className='genreItems'>{item}</div>
+        <div className='genreItems' onClick={() => test(item)}>{item}</div>
    ))
 
     return(
         <div className='genreSidebar'>
+            <h3 id='genresText'>Genres</h3>
             <div className='genreItemsContainer'>
                 {genreList}
             </div>
-             
+
         </div>
     )
 }
