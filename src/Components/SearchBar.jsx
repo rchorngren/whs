049fda076472eca91875399a56c75c62390deb17
johanstahre <Johan@bk30.se>
2021-05-13 20:55:40 +0200

    created nescessary components

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchResultRow from './SearchResultRow'
+
+function SearchBar() {
+    return (
+        <>
+        <input placeholder="Search..." style={{
+            width: "120px", 
+            height: "20px"
+        }}/>
+        </>
+    )
+}
+
+export default SearchBar
---
+++ src/Components/SearchResultRow.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function SearchResultRow() {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <img alt="movie poster" width="120" src="https://m.media-amazon.com/images/M/MV5BMTg1MTY2MjYzNV5BMl5BanBnXkFtZTgwMTc4NTMwNDI@._V1_.jpg" />
+                    </td>
+                    <td>
+                        <h3>Black Panther</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor vitae lacus ac aliquet. Praesent orci sapien, elementum eget tempor a, finibus nec purus.</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default SearchResultRow
