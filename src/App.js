1f594d8b14162a0b443c638f3a82a85bb87b3423
Robert Horngren <robert@horngren.net>
2021-05-13 15:50:57 +0200

    rebased main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import CounterRedux from './Components/CounterRedux';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        WHS
-      </header>
+      <Header />
       <main>
         <CounterRedux />
         <CounterRedux />
         <CounterRedux />
       </main>
+      <Footer />
     </div>
   );
 }
