a7164feb53a2c1eeaa2e4ff3736c4787eca3875b
Marek <repinski@hotmail.com>
2021-05-14 13:38:44 +0200

    API for search done

--- src/Features/repositoryAPI.js
+++ src/Features/repositoryAPI.js
@@ -82,6 +82,11 @@ export async function getGenreMovieList(genreId, page) {
 /*                            getSortedFlix() - Async                                 */
 /*                                                                                    */
 /*  Returns a list of movies (20/page) matching a searchcriteria from db as JSON      */
+/*  Parameters: (search String, multi Bool, page Int)                                 */
+/*         search: 'popularity' = get most popular movies                             */
+/*                 'recommended' = get movies with highest vote                       */
+/*                 'new' = get newest movies                                          */
+/*         page: the number of the page                                               */
 /*                                                                                    */
 /*  JSON format: {"page": Int, "total_pages": Int, "total_results": Int,              */
 /*                  "results": [                                                      */
@@ -106,11 +111,10 @@ export async function getGenreMovieList(genreId, page) {
 /* import { getSortedFlix } from "./Features/repositoryAPI";                          */
 /* ...                                                                                */
 /* let myArray = '';                                                                  */
-/* let dispatch = null;                                                               */
 /* let search = 'popularity'; //'popularity' is defalt, other: 'recommended' or 'new' */
 /* let currPage = 1;                                                                  */
 /* getSortedFlix(search, currPage).then((resp) => { myArray = JSON.parse(resp) });    */
-/* let content = <div> myArray.genres[i].title </div>                                 */
+/* let content = <div> myArray.results[i].title </div>                                */
 /* ...                                                                                */
 /**************************************************************************************/
 export async function getSortedFlix(search, page) {
@@ -136,20 +140,78 @@ export async function getSortedFlix(search, page) {
 }
 
 /**************************************************************************************/
-/*                              getFromDb() - Async                                   */
+/*                             searchFlix() - Async                                   */
+/*  Parameters: (search String, multi Bool, page Int)                                 */
+/*         search: the string to search for                                           */
+/*         multi: if false it will only search for movies                             */
+/*                if true it will search for movies, tv shows and actors              */
+/*         page: the number of the page                                               */
 /*                                                                                    */
-/*  Send API question with parameter 'url' and return JSON                            */
+/*  Returns a list of movies (20/page) matching a searchcriteria from db as JSON      */
 /*                                                                                    */
+/*  JSON format: {"page": Int, "total_pages": Int, "total_results": Int,              */
+/*                  "results": [                                                      */
+/*                      "id": Int,                                                    */
+/*                      "title": String,                                              */
+/*                      "original_title": String,                                     */
+/*                      "original_language": String,                                  */
+/*                      "overview": String,                                           */
+/*                      "poster_path": String,                                        */
+/*                      "release_date": Date,                                         */
+/*                      "popularity": Int,                                            */
+/*                      "vote_average": Float,                                        */
+/*                      "vote_count": Int,                                            */
+/*                      "video": Bool,                                                */
+/*                      "adult": Bool,                                                */
+/*                      "backdrop_path": String,                                      */
+/*                      "media_type": String,                                         */
+/*                      "genre_ids": [Int, Int, Int]                                  */
+/*                  ]                                                                 */
+/*               }                                                                    */
+/*  Usage:                                                                            */
+/*                                                                                    */
+/*                                                                                    */
+/* import { searchFlix } from "./Features/repositoryAPI";                             */
+/* ...                                                                                */
+/* let myArray = '';                                                                  */
+/* let search = 'star wars';                                                          */
+/* let multi = false;                                                                 */
+/* let currPage = 1;                                                                  */
+/* searchFlix(search, multi, currPage).then((r) => { myArray = JSON.parse(r) });      */
+/* let content = <div> myArray.results[i].title </div>                                */
+/* ...                                                                                */
 /**************************************************************************************/
-async function getFromDb(url) {
-  try {
-    let resp = await fetch(url);
-    let data = await resp.json();
+export async function searchFlix(search, multi, page) {
+  let parametersOk = true;
+  let options = 'search/multi?' + apiKey1Lang;
 
-    return JSON.stringify(data);
+  if (!multi) {
+    options = 'search/movie?' + apiKey1Lang;
   }
-  catch (error) {
-    console.log(error);
-    return false;
+
+  if (search === '' && search.length < 2) {
+    parametersOk = false;
+  } else {
+    options += '&query=' + search;
+  }
+
+  if (isNaN(page)) {
+    options += '&page=1';
+  } else {
+    options += '&page=' + page;
+  }
+
+  if (parametersOk) {
+    try {
+      let resp = await fetch(url1 + options);
+      let data = await resp.json();
+
+      return JSON.stringify(data);
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 }
+
