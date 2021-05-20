44cdeb5df4d61a7007378d14e61d491d3a93a95e
johanstahre <Johan@bk30.se>
2021-05-20 12:26:02 +0200

    rebase from main

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,6 +5,9 @@ import { actions, ACTIVEVIEW } from '../../Features/activeView';
 import BasketCounter from '../BasketCounter/BasketCounter';
 import ShoppingCart from '../../Assets/Images/shoppingCart.png';
 import './Header.css';
+import SearchBar from '../SearchBar';
+import SearchResultRow from '../SearchResultRow';
+import { searchFlix } from '../../Features/repositoryAPI';
 
 const Header = () => {
     const [shoppingCartButtonIsClicked, setShoppingCartButtonIsClicked] = useState(false);
@@ -41,7 +44,31 @@ const Header = () => {
         }, 250);
     }
 
+<<<<<<< HEAD
+=======
+    //searchfunction
+    const [movie, setMovieData] = useState([]);
+    const [searchTerm, SetSearchTerm] = useState([]);
+    let multi = false
+    let currPage = 1;
+    let search = searchTerm;
+    
+
+      useEffect(() => {
+        searchFlix(search, multi, currPage).then((r) => {
+            if(search.length < 2) {
+                // console.log('no data')
+            } else {
+                setMovieData(JSON.parse(r))
+                console.log(movie)
+            }  
+        })
+        
+      }, [searchTerm])
+
+>>>>>>> rebase from main
     return (
+        <>
         <header className="header">
             <div
                 className="menuButton buttonGeneral"
@@ -66,10 +93,16 @@ const Header = () => {
 
             </div>
 
+<<<<<<< HEAD
             <div style={{marginRight: '-10px'}}>
                 <h2>WHS</h2>
             </div>
 
+=======
+            <h2>WHS</h2>
+            
+            <SearchBar searchTerm={searchTerm} SetSearchTerm={SetSearchTerm}/>
+>>>>>>> rebase from main
 
             <div
                 className="shoppingcartButton buttonGeneral"
@@ -79,6 +112,8 @@ const Header = () => {
                     src={ShoppingCart} alt="" />
             </div>
         </header>
+        {<SearchResultRow movie={movie} />}
+        </>
     )
 }
 
