f90b5feeb7ccceadbcadaeda351e7d41ea66ecac
Marek <repinski@hotmail.com>
2021-05-20 16:00:44 +0200

    slow start

--- src/Components/LoadingAnim.jsx
+++ src/Components/LoadingAnim.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import logo from '../logo.png';
 import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import './loadingAnim.css';
+
+const status = useSelector(state => state.genresListOf.status);
+const dispatch = useDispatch();
+
+useEffect(() => {
+  if (status === GENSTATUS.NORMAL) {
+    getImgUrl();
+    getGenre(dispatch);
+
+  }
+}, [dispatch]);
 
 const LoadingAnim = () => {
     return(
-        <div className='not-loading'>Loading</div>
+        <div className='loading'>Loading</div>
     );
 }
 
