5be52688824f935564ba718c012c59f82a52770a
Erizium <tobias.osterlin@gmail.com>
2021-05-20 11:19:59 +0200

    changed variable name from testMapping to genreListMap

--- src/Components/GenreSidebar/GenreMenu.jsx
+++ src/Components/GenreSidebar/GenreMenu.jsx
@@ -9,15 +9,15 @@ const GenreMenu = () => {
     const status = useSelector(state => state.genresListOf.status);
     const genreList = JSON.parse(useSelector(state => state.genresListOf.list));
 
-    let testMapping = [];
+    let genreListMap = [];
     if(status === STATUS.SUCCESS) {
         console.log(genreList);
 
-        testMapping = genreList.genres.map((genre) => (
+        genreListMap = genreList.genres.map((genre) => (
             <div className='genreItems' key={genre.name} onClick={() => { console.log(genre.name) }}>{genre.name}</div>
 
         ))
-        console.log(testMapping)
+        console.log(genreListMap)
     }
 
     return (
@@ -26,7 +26,7 @@ const GenreMenu = () => {
             <h3 id='genresText'>Genres</h3>
                 <div className='sidebarScroll'>
                     <div className='genreItemsContainer'>
-                        {testMapping}
+                        {genreListMap}
                     </div>
                 </div>               
         </div>  
