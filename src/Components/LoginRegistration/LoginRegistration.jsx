568853f77ad0b17d58c5195296701bbb89d727e1
Robert Horngren <robert@horngren.net>
2021-05-20 16:11:37 +0200

    added view and updated styling

--- src/Components/LoginRegistration/LoginRegistration.jsx
+++ src/Components/LoginRegistration/LoginRegistration.jsx
@@ -9,6 +9,7 @@ const LoginRegistration = () => {
     const [userName, setUsername] = useState('');
     const [userPassword, setUserPassword] = useState('');
     const [userPasswordConfirm, setUserPasswordConfirm] = useState('');
+    const [registrationComplete, setRegistrationComplete] = useState(false);
 
     const loggedin = useSelector(state => state.loggedinUser);
 
@@ -50,6 +51,7 @@ const LoginRegistration = () => {
                     console.log('userCredential: ', userCredential);
                     localStorage.setItem('currentUser', JSON.stringify(userCredential));
                     dispatch(actions.loggedin());
+                    setRegistrationComplete(true);
                 })
                 .catch((error) => {
                     let errorCode = error.code;
@@ -62,34 +64,59 @@ const LoginRegistration = () => {
         }
     }
 
-    return (
-        // <div style={style.loginComponent}>
-        <div className="loginComponent">
-            {showLogin ? (
-                <div className="loginContainer">
-                    <h3>Login</h3>
+    function loginView() {
+        return (
+            <div className="login-registration-container">
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <input type='text' className="textInput" placeholder="E-mail" value={userName} onInput={e => setUsername(e.target.value)} />
                     <input type='password' className="textInput" placeholder="Password" value={userPassword} onInput={e => setUserPassword(e.target.value)} />
-                    <div className="loginButton" onClick={() => loginUser()}>Login</div>
-                    <div className="newCustomerText" onClick={() => toggleLogin()}>
-                        Not yet a customer? <br />
-                        Click here to sign up
-                    </div>
                 </div>
-            ) : (
-                <div className="loginContainer">
-                    <h3>Registration</h3>
+
+                <div className="call-to-action-button" onClick={() => loginUser()}>Login</div>
+                <div className="newCustomerText" onClick={() => toggleLogin()}>
+                    New to WHS? Register here!
+                    </div>
+            </div>
+        )
+    }
+
+    function registrationView() {
+        return (
+            <div className="login-registration-container">
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <input type='text' className="textInput" placeholder="E-mail" value={userName} onInput={e => setUsername(e.target.value)} />
                     <input type='password' className="textInput" placeholder="Password" value={userPassword} onInput={e => setUserPassword(e.target.value)} />
                     <input type='password' className="textInput" placeholder="Confirm password" value={userPasswordConfirm} onInput={e => setUserPasswordConfirm(e.target.value)} />
-                    <div className="registerButton" onClick={() => registerUser()}>Register</div>
-                    <div className="newCustomerText" onClick={() => toggleLogin()}>
-                        Back to login
-                    </div>
                 </div>
+
+                <div className="call-to-action-button" onClick={() => registerUser()}>Register</div>
+                <div className="newCustomerText" onClick={() => toggleLogin()}>
+                    Back to login
+                </div>
+            </div>
+        )
+    }
+
+    function registrationDoneView() {
+        return (
+            <div className="login-registration-container">
+                <h3>Thank you for choosing WHS!</h3>
+                <div className="call-to-action-button" onClick={() => console.log('go to main view')}>Let the adventure begin!</div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="loginComponent">
+            {showLogin ? (
+                loginView()
+            ) : (
+                registrationComplete ? (
+                    registrationDoneView()
+                ) : (
+                    registrationView()
+                )
             )}
-
-
         </div>
     )
 }
