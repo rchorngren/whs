d19be98b7081ba0de52d7ccfdfc20e1642d67241
johanstahre <Johan@bk30.se>
2021-05-25 00:19:03 +0200

    minior fixes

--- src/Components/SearchResultRow/SearchResultRow.jsx
+++ src/Components/SearchResultRow/SearchResultRow.jsx
@@ -3,30 +3,30 @@ import './SearchResultRow.css'
 
 const SearchResultRow = (props) => {
     const url = 'https://image.tmdb.org/t/p/w200';
-    useEffect(() => {
-       try {
-           if (props.movie.results) {
-               console.log(props.movie.results)
-           } else {
-               console.log('no data in movie')
-           }
-       } catch (e) {
-           console.log(e)
-       }
-    })
+    // useEffect(() => {
+    //    try {
+    //        if (props.movie.results) {
+    //            console.log(props.movie.results)
+    //        } else {
+    //            console.log('no data in movie')
+    //        }
+    //    } catch (e) {
+    //        console.log(e)
+    //    }
+    // })
 
     try {
         if (props.movie.results) {
             return (
                 <>
                 {props.movie.results.map((movie, index) => 
-                <table className="results">
+                <table className="results" key={index}>
                     <tbody>
                         <tr>
                             <td>
                                 <img alt="movie poster" src={url + props.movie.results[index].poster_path} />
                             </td>
-                            <td style={{textAlign: 'left'}}>
+                            <td>
                                 <h3>{props.movie.results[index].title}</h3>
                                 <p>{props.movie.results[index].overview}</p>
                             </td>
