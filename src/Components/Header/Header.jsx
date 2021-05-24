05e5aea052a4a5d5209d752dbb62208212927914
Robert Horngren <robert@horngren.net>
2021-05-24 14:42:32 +0200

    code cleaning

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,14 +7,9 @@ import ShoppingCart from '../../Assets/Images/shoppingCart.png';
 import './Header.css';
 
 const Header = () => {
-    const [itemsInBasket] = useState(1);
     const [shoppingCartButtonIsClicked, setShoppingCartButtonIsClicked] = useState(false);
     const [menuButtonIsClicked, setMenuButtonIsClicked] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
-    
-    const loggedIn = useSelector(state => state.loggedinUser.loggedinUser);
-    const [userLoggedIn, setUserLoggedIn] = useState(loggedIn);
-
     const activeView = useSelector(state => state.activeView.activeView);
 
     useEffect(() => {
@@ -24,10 +19,6 @@ const Header = () => {
             setMenuOpen(false);
         }
     }, [activeView]);
-
-    // useEffect(() => {
-    //     setUserLoggedIn(loggedIn);
-    // }, [loggedIn]);
 
     const dispatch = useDispatch();
 
@@ -49,32 +40,6 @@ const Header = () => {
             }
         }, 250);
     }
-
-    //Resets the styling first and then triggers the function of the button
-    // function animationOnClick(dispatch, clickedButton) {
-    //     setTimeout(() => {
-    //         setProfileButtonIsClicked(false);
-    //     }, 100);
-    //     setTimeout(() => {
-    //         if (clickedButton === 'baskedClicked') {
-    //             setMenuButtonIsClicked(false);
-    //             if (userLoggedIn) {
-    //                 // dispatch(actions.profile());
-    //                 dispatch(actions.checkout());
-    //             } else {
-    //                 dispatch(actions.login());
-    //             }
-    //         } else {
-    //             if (menuButtonIsClicked) {
-    //                 setMenuButtonIsClicked(!menuButtonIsClicked);
-    //                 dispatch(actions.empty());
-    //             } else {
-    //                 dispatch(actions.menu());
-    //             }
-    //         }
-    //     }, 250);
-    // }
-
 
     return (
         <header className="header">
@@ -103,26 +68,12 @@ const Header = () => {
 
             <h2>WHS</h2>
 
-            {/* <div
-                className={userLoggedIn ? (
-                    //styling if user is logged in
-                    "profileButtonLoggedIn buttonGeneral"
-                ) : (
-                    //styling if user is not logged in
-                    "profileButton buttonGeneral"
-                )}
-                onClick={() => { setProfileButtonIsClicked(true); animationOnClick(dispatch, 'profileClicked') }}>
-                <img
-                    className={profileButtonIsClicked ? "buttonImageClicked" : "buttonImage"}
-                    src={ProfileIcon} alt="" />
-            </div> */}
             <div
                 className="shoppingcartButton buttonGeneral"
                 onClick={() => { setShoppingCartButtonIsClicked(true); animationOnClick(dispatch, 'baskedClicked') }}>
                 <img
                     className={shoppingCartButtonIsClicked ? "buttonImageClicked" : "buttonImage"}
                     src={ShoppingCart} alt="" />
-                {/* {itemsInBasket > 0 ? <BasketCounter /> : null} */}
             </div>
         </header>
     )
