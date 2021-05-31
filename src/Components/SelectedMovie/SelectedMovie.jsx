7237a27324077cc378d8f46f919c508c4632e40e
Robert Horngren <robert@horngren.net>
2021-05-31 16:22:57 +0200

    added catch for when movie poster isn't found

--- src/Components/SelectedMovie/SelectedMovie.jsx
+++ src/Components/SelectedMovie/SelectedMovie.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import ScrollContainer from 'react-indiana-drag-scroll';
 import { getFlixDetail } from "../../Features/repositoryAPI";
 import './SelectedMovie.css';
+import logo from '../../logo.png';
 
 const SelectedMovie = () => {
     const url = 'https://image.tmdb.org/t/p/w200';
@@ -20,15 +21,15 @@ const SelectedMovie = () => {
 
             <div className="movie-info">
                 <div className="movie-poster">
-                    <img src={url + flixDetail.poster_path} alt="" />
+                    {flixDetail ? (
+                        <img src={url + flixDetail.poster_path} alt="" />
+                    ) : (null)}
                 </div>
-
 
                 <div className="movie-info-container">
                     <div className="movie-title-text">{flixDetail.original_title}</div>
                     <div className="movie-info-text">{flixDetail.overview}</div>
                 </div>
-
             </div>
 
             <div style={{ display: 'flex', flexDirection: 'row', marginTop: '20px' }}>
@@ -36,6 +37,7 @@ const SelectedMovie = () => {
                     User rating <br />
                     <div className="user-rating-score-text">4.5</div>
                 </div>
+
                 <div className="movie-price-text">
                     Price <br />
                     $4.99
@@ -50,7 +52,6 @@ const SelectedMovie = () => {
                 <div className="user-review-header">User reviews</div>
 
                 {/* user-review is generated through mapping of any reviews left by other users */}
-
                 <div className="user-review">
                     <div className="user-review-text">
                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
