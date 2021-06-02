a0bc09eaf65850b1d08817e2dc837460cf7fb80e
Robert Horngren <robert@horngren.net>
2021-06-02 11:02:50 +0200

    basket wip

---
+++ src/Features/customerBasket.js
@@ -0,0 +1,36 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+const addItem = createAction('add item');
+const removeItem = createAction('remove item');
+const content = createAction('content of basket')
+
+const actions = {
+    addItem,
+    removeItem,
+    content
+}
+
+// const BASKET = {
+//     CONTENT: 'content of basket'
+// }
+
+const initialState = {
+    content: []
+}
+
+const reducer = createReducer(initialState, {
+    [addItem]: (state, action) => ({
+        ...state,
+        content: action.payload
+    }),
+    // [removeItem]: (state, action) => ({
+    //     ...state,
+    //     content: ''
+    // }),
+    [content]: (state, action) => ({
+        ...state,
+        content: []
+    }),
+})
+
+export { actions, reducer }
