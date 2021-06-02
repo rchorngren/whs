d3213e1ad154c4749019a453143eee9b0d2ad926
Robert Horngren <robert@horngren.net>
2021-06-02 13:25:50 +0200

    wip

--- src/Components/BasketView/BasketView.jsx
+++ src/Components/BasketView/BasketView.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './BasketView.css';
 import remove from '../../Assets/Images/cross.svg';
+import { useSelector } from 'react-redux';
+
+
 
 const Basket = () => {
 
     const [buttonClicked, setButtonClicked] = useState(false);
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    const currentBasket = useSelector(state => state.customerBasket.content);
 
     function goToCheckout() {
         console.log('going to checkout');
@@ -24,6 +30,36 @@ const Basket = () => {
         }, 250);
     }
 
+    const mapBasket = () => {
+        const basket = currentBasket;
+
+        // basket.map((item, index) => {
+        //     console.log('item in basket: ', item.movieTitle);
+        // })
+        
+        basket.forEach(item => {
+            console.log('item in basket: ', item.price);
+        });
+    }
+
+    function calculateTotal() {
+
+        currentBasket.forEach(item => {
+            let currentTotal = totalPrice;
+            let price = item.price;
+            console.log('totalPrice: ', totalPrice);
+            setTotalPrice(currentTotal + price);
+        });
+        
+    }
+
+    useEffect(() => {
+        calculateTotal();
+        console.log('basket view loaded');
+        console.log('currentBasket: ', currentBasket);
+        // mapBasket()
+    }, []);
+
     return (
         <div className="basket-view-component">
             <div className="basket-view-container">
@@ -32,9 +68,11 @@ const Basket = () => {
                     
 
                     <div className="movies-in-basket">
-
+                        {mapBasket()}
+                        
                         {/* items in basket goes here - placeholders atm */}
 
+{/* 
                         <div className="basket-item-container">
                             <div className="basket-item-title">A really cool movie title</div>
                             <div className="basket-item-price">$4.99</div>
@@ -58,8 +96,8 @@ const Basket = () => {
                             <div className="basket-item-title">A really cool movie title</div>
                             <div className="basket-item-price">$4.99</div>
                             <img className="basket-item-trash" src={remove} alt="" onClick={() => trashItem()} />
+                        </div> */}
 
-                        </div>
 
                         {/* end of items */}
 
@@ -67,7 +105,7 @@ const Basket = () => {
 
                     <div className="basket-summary">
                         <div style={{ marginLeft: '5px' }}>Total:</div>
-                        <div style={{ marginRight: '5px' }}>$19.96</div>
+                        <div style={{ marginRight: '5px' }}>${totalPrice}</div>
                     </div>
 
                 </div>
