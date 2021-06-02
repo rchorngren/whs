e9ec0fdc7d96ba0f8ccba9c7dae93949f9406bdb
Robert Horngren <robert@horngren.net>
2021-06-02 15:24:51 +0200

    added total value of basket

--- src/Components/BasketView/BasketView.jsx
+++ src/Components/BasketView/BasketView.jsx
@@ -6,19 +6,21 @@ import { useSelector } from 'react-redux';
 
 
 const Basket = () => {
-
     const [buttonClicked, setButtonClicked] = useState(false);
     const [totalPrice, setTotalPrice] = useState(0);
+    const [contentOfBasket, setContentOfBasket] = useState(null);
 
     const currentBasket = useSelector(state => state.customerBasket.content);
+
+    let priceArray = [];
 
     function goToCheckout() {
         console.log('going to checkout');
         //dispatch to checkout component goes here
     }
 
-    function trashItem() {
-        console.log('throwing away...');
+    function trashItem(id) {
+        console.log('throwing away...', id);
     }
 
     function animationOnClick() {
@@ -30,76 +32,40 @@ const Basket = () => {
         }, 250);
     }
 
-    const mapBasket = () => {
-        const basket = currentBasket;
-
-        // basket.map((item, index) => {
-        //     console.log('item in basket: ', item.movieTitle);
-        // })
-        
-        basket.forEach(item => {
-            console.log('item in basket: ', item.price);
-        });
+    function calculateTotalPrice(total, num) {
+        return total + num;
     }
 
-    function calculateTotal() {
-
-        currentBasket.forEach(item => {
-            let currentTotal = totalPrice;
-            let price = item.price;
-            console.log('totalPrice: ', totalPrice);
-            setTotalPrice(currentTotal + price);
-        });
+    const buildBasket = () => {
         
+        setContentOfBasket(currentBasket.map((item, index) => {            
+            priceArray.push(item.price);
+            setTotalPrice(priceArray.reduce(calculateTotalPrice));
+
+            return (
+                <div className="basket-item-container" key={index}>
+                    <div className="basket-item-title">{item.movieTitle}</div>
+                    <div className="basket-item-price">${item.price}</div>
+                    <img className="basket-item-trash" src={remove} alt="" onClick={() => trashItem(item.movieId)} />
+                </div>
+            )
+        }))
     }
 
     useEffect(() => {
-        calculateTotal();
-        console.log('basket view loaded');
-        console.log('currentBasket: ', currentBasket);
-        // mapBasket()
-    }, []);
+        buildBasket();
+    }, [currentBasket]);
 
     return (
         <div className="basket-view-component">
             <div className="basket-view-container">
                 <div className="basket-header-text">Your basket</div>
                 <div className="items-in-basket-container">
-                    
+
 
                     <div className="movies-in-basket">
-                        {mapBasket()}
-                        
-                        {/* items in basket goes here - placeholders atm */}
 
-{/* 
-                        <div className="basket-item-container">
-                            <div className="basket-item-title">A really cool movie title</div>
-                            <div className="basket-item-price">$4.99</div>
-                            <img className="basket-item-trash" src={remove} alt="" onClick={() => trashItem()} />
-                        </div>
-
-                        <div className="basket-item-container even">
-                            <div className="basket-item-title">A really cool movie title</div>
-                            <div className="basket-item-price">$4.99</div>
-                            <img className="basket-item-trash" src={remove} alt="" onClick={() => trashItem()} />
-                        </div>
-
-                        <div className="basket-item-container">
-                            <div className="basket-item-title">A really cool movie title</div>
-                            <div className="basket-item-price">$4.99</div>
-                            <img className="basket-item-trash" src={remove} alt="" onClick={() => trashItem()} />
-
-                        </div>
-
-                        <div className="basket-item-container even">
-                            <div className="basket-item-title">A really cool movie title</div>
-                            <div className="basket-item-price">$4.99</div>
-                            <img className="basket-item-trash" src={remove} alt="" onClick={() => trashItem()} />
-                        </div> */}
-
-
-                        {/* end of items */}
+                        {contentOfBasket}
 
                     </div>
 
