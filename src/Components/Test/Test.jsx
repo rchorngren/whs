af4bdb9ddfbc34e49282c58e71e6e5434446ece1
Marek <repinski@hotmail.com>
2021-06-03 20:46:21 +0200

    create order done as const

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -4,25 +4,16 @@ import './test.css';
 
 const Test = () => {
     const [content, setContent] = useState('');
-    const userCred = JSON.parse(localStorage.currentUser);
-    console.log('user ' + userCred.user);
-    
+
     useEffect(() => {
-        console.log('uid ' + userCred.user.uid);
-        console.log('name ' + userCred.user.email);
-        let orderList = [(1, 603), (1, 605)];
-        
-        let tempElements = orderList.map((movie, index) => (
-            console.log(movie)
-        ));
-        // FirestoreService.createOrder(orderList)
-        // .then(docRef => {
-        //     setContent(docRef.id);
-        // })
-        // .catch(reason => setContent('create-list-error' +  reason));    
+        FirestoreService.createOrder([603, 605])
+        .then(() => {
+            console.log('finnished?');
+        })
+        .catch(reason => setContent('create-list-error' +  reason));    
     }, []);
-    
-    return(
+
+    return (
         <div className='test'>{content}</div>
     );
 }
--- src/Features/repositoryFS.js
+++ src/Features/repositoryFS.js
@@ -1,20 +1,68 @@
+/**************************************************************************************/
+/*                              repositoryFS.js                                       */
+/*                                                                                    */
+/*  A collenction  of useful functionen used to get data from Firestore.              */
+/**************************************************************************************/
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 const db = firebase.firestore();
 
-export const createOrder = (moviesList) => {
-    if (moviesList.lenght){
-
+export const createOrder = (movieIDs) => {
+    if (checkMovieIDs(movieIDs)) {
+        let userId = 'Li4sUlGWF2fYe0Yy8oh7lLSIzpi1';
+        let totalPrice = 4.99;
+        if (Array.isArray(movieIDs)) {
+            totalPrice = movieIDs.length * 4.99;
+        }
+        if (localStorage.currentUser !== undefined) {
+            let userCred = JSON.parse(localStorage.currentUser);
+            userId = userCred.user.uid;
+        }
+        db.collection('Users').doc(userId).collection('Orders')
+            .add({
+                created: firebase.firestore.FieldValue.serverTimestamp(),
+                total: totalPrice
+            })
+            .then(docRef => {
+                console.log(docRef.id);
+                if (Array.isArray(movieIDs)) {
+                    for (let i = 0; i < movieIDs.length; i++) {
+                        db.collection('Users').doc(userId).collection('Orders').doc(docRef.id).collection('Items')
+                            .add({
+                                created: firebase.firestore.FieldValue.serverTimestamp(),
+                                movieID: movieIDs[i],
+                                price: 4.99
+                            })
+                    }
+                }
+                else {
+                    db.collection('Users').doc(userId).collection('Orders').doc(docRef.id).collection('Items')
+                        .add({
+                            created: firebase.firestore.FieldValue.serverTimestamp(),
+                            movieID: movieIDs,
+                            price: 4.99
+                        })
+                }
+            })
     }
-    // return db.collection('groceryLists')
-    //     .add({
-    //         created: firebase.firestore.FieldValue.serverTimestamp(),
-    //         createdBy: userId,
-    //         users: [{ 
-    //             userId: userId,
-    //             name: userName
-    //         }]
-    //     });
 };
+
+function checkMovieIDs(arr) {
+    if (Array.isArray(arr)) {
+        for (let i = 0; i < arr.length; i++) {
+            if (isNaN(arr[i])) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        if (isNaN(arr)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
