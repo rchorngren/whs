12925bd7703bcb669c6e4429d508e4c814e939d2
Erizium <tobias.osterlin@gmail.com>
2021-06-07 09:55:28 +0200

    Can now see price and movies in cart

--- src/Components/Checkout/CheckoutView.jsx
+++ src/Components/Checkout/CheckoutView.jsx
@@ -3,13 +3,54 @@ import React from 'react';
 import './CheckoutView.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { actions } from '../../Features/customerBasket';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const CheckoutView = () => {
  
+    const [contentOfBasket, setContentOfBasket] = useState(null);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const currentBasket = useSelector(state => state.customerBasket.content);
 
-    
+    let priceArray = [];
+
+    function calculateTotalPrice(total, num) {
+        return total + num;
+    }
+
+    const buildBasket = () => {
+        setContentOfBasket(currentBasket.map((item, index) => {
+            priceArray.push(item.price);
+            let priceToSet = priceArray.reduce(calculateTotalPrice);
+            let roundedPrice = Math.round((priceToSet + Number.EPSILON) * 100) / 100;
+            setTotalPrice(roundedPrice);
+
+            if(index % 2 === 0) {
+                return(
+                    <div className="basket-item-container" key={index}>
+                        <div className="basket-item-title">{item.movieTitle}</div>
+                        <div className="basket-item-price">${item.price}</div>
+                    </div>
+                )
+            } else {
+                return(
+                    <div className="basket-item-container uneven" key={index}>
+                    <div className="basket-item-title">{item.movieTitle}</div>
+                    <div className="basket-item-price">${item.price}</div>
+                </div>
+                )
+            }
+        }));
+    }
+
+    useEffect (() => {
+        buildBasket();
+        if(currentBasket.length === 0){
+            setTotalPrice(0);
+        }
+        
+
+    }, [currentBasket]); 
 
 
     return (
@@ -17,10 +58,10 @@ const CheckoutView = () => {
             <div className='checkoutMovies'>
                 <text>Checkout</text>
                 <div className='moviesInCart'>
-                    <div className='cart'>Movies</div>
+                    <div className='cart'>{contentOfBasket}</div>
                     <div id='price'>
                         <div className='totalPrice'>Total: </div>
-                        <div className='combinedPrice'>10.00</div>
+                        <div className='combinedPrice'>{totalPrice}</div>
                     </div>
                 </div>
                 <div className='emailInput'>
