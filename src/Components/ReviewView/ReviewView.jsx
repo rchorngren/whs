a973e06aef5f64c28c438220aed020282ec9a6b7
johanstahre <Johan@bk30.se>
2021-06-08 11:07:20 +0200

    added star rating styling

--- src/Components/ReviewView/ReviewView.jsx
+++ src/Components/ReviewView/ReviewView.jsx
@@ -3,10 +3,62 @@ import './ReviewView.css'
 
 
 function ReviewView() {
-    const [reviewActive, setreviewActive] = useState(false)
-    const [isChecked, setIsChecked] = useState(false)
+    const [reviewActive, setreviewActive] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
+    const [rating, setRating] = useState(0);
+    const [hoverRating, setHoverRating] = useState(0);
+    const ratingStars = [1, 2, 3, 4, 5]
 
+    function StarIcon (props) {
+        const {fill = 'none'} = props
+        return (
+            <svg className="w-6 h-6" fill={fill} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
+            </svg>
+        )
+    }
+
+    function Rating(props) {
+        const {
+            index,
+            rating, 
+            hoverRating,
+            onMouseEnter,
+            onMouseLeave,
+            onSaveRating
+        } = props;
+        const fill = React.useMemo(() => {
+            if (hoverRating >= index) {
+                return 'yellow';
+            } else if (!hoverRating && rating >= index){
+                return 'yellow';
+            }
+            return 'none';
+        }, [rating, hoverRating, index])
+        return (
+            <div 
+            onMouseEnter={() => onMouseEnter(index)} 
+            onMouseLeave={() => onMouseLeave()} 
+            onClick={() => onSaveRating(index)}>
+            <StarIcon fill={fill} />
+            </div>
+        )
+    }
+
+    const onMouseEnter = (index) => {
+        setHoverRating(index);
+        console.log('hovering ' + index + 'stars')
+    }
     
+    const onMouseLeave = () => {
+        setHoverRating(0);
+        console.log('ends hovering')
+    }
+
+    const onSaveRating = (index) => {
+        setRating(index)
+        console.log('clicked star: ' + index)
+    }
     if(reviewActive) {
         return (
             <div className="review-view-component">
@@ -33,6 +85,22 @@ function ReviewView() {
                 <div className="review-subheader-text">Submit your review of 'INSERT MOVIE TITLE' here</div>
                 <div className="review-container">
                     <textarea className="review-input-field" placeholder="Leave your review here..." />
+                </div>
+                <div className="review-subheader-text">Rate the Movie</div>
+                {/* rating system here */}
+                <div className="review-rating-container">
+                    {[1,2,3,4,5].map((index) => {
+                        return (
+                            <Rating 
+                            index={index} 
+                            rating={rating} 
+                            hoverRating={hoverRating} 
+                            onMouseEnter={onMouseEnter} 
+                            onMouseLeave={onMouseLeave} 
+                            onSaveRating={onSaveRating} />
+                        )
+                    })}
+                    
                 </div>
                 <div className="review-terms">
                     <input type="checkbox" className="review-checkbox" onChange={() => setIsChecked(!isChecked)} />
