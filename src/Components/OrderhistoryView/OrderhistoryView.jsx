ab096e67ddcd6bf67c849375b16e3d7b40e2e51e
Robert Horngren <robert@horngren.net>
2021-06-08 10:29:53 +0200

    wip

--- src/Components/OrderhistoryView/OrderhistoryView.jsx
+++ src/Components/OrderhistoryView/OrderhistoryView.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState} from 'react';
+import { getOrders } from '../../Features/repositoryFS';
 import './OrderhistoryView.css';
 import { actions } from '../../Features/activeView';
 import { useDispatch } from 'react-redux';
@@ -5,10 +7,33 @@ import { useDispatch } from 'react-redux';
 const OrderhistoryView = () => {
     const dispatch = useDispatch();
 
+    const [response, setResponse] = useState('');
+    const [content, setContent] = useState(null);
+
+    useEffect(() => {
+        getOrders().then((resp) => {
+            setResponse(JSON.parse(resp));
+        }) // eslint-disable-next-line;
+    }, []);
+
+    useEffect(() => {
+        if (response !== ''){
+            if (response == null){
+                setContent('User not logged in');
+            } else if (response.orders.length === 0){
+                setContent('No orders');
+            } else {
+                setContent('Date of first order: ' + response.orders[0].date);
+                console.log('response: ', response.orders);
+            }
+        }
+    }, [response]);
+
+
     return (
         <div className="orderhistory-container">
-                        
-            <div className="order-container">
+            {content}
+            {/* <div className="order-container">
                 <div className="order-individual-movie">
                     <div className="order-info-text">The Matrix</div>
                     <div className="order-info-text">Price: $4.99   <button onClick={() => {
@@ -29,7 +54,7 @@ const OrderhistoryView = () => {
                     <div>Total price:</div>
                     <div>$9.98</div>
                 </div>
-            </div>
+            </div> */}
 
         </div>
     )
