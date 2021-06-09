640ce3411a84167973c2ab7c81b98e90614f969c
Erizium <tobias.osterlin@gmail.com>
2021-06-09 09:35:17 +0200

    Added animation to Pay and Thanks buttons in CheckoutView and CheckoutThanks respectively

--- src/Components/Checkout/CheckoutView.jsx
+++ src/Components/Checkout/CheckoutView.jsx
@@ -9,6 +9,7 @@ import{ actions as activeViewActions } from '../../Features/activeView';
 
 const CheckoutView = () => {
  
+    const [buttonClicked, setButtonClicked] = useState(false);
     const [contentOfBasket, setContentOfBasket] = useState(null);
     const [totalPrice, setTotalPrice] = useState(0);
     const currentBasket = useSelector(state => state.customerBasket.content);
@@ -57,6 +58,15 @@ const CheckoutView = () => {
         createOrder(orderList, titleList, () => setFsQueryDone(true));
     }
 
+    function animationOnClick() {
+        setTimeout(() => {
+            setButtonClicked(false);
+        }, 150);
+        setTimeout(() => {
+            makePurchase()
+        }, 250);
+    }
+
     useEffect (() => {
         buildBasket();
         if(currentBasket.length === 0){
@@ -99,8 +109,11 @@ const CheckoutView = () => {
                     </div>
                 </div>
 
-            <button className='payButton'  
-                onClick={() => makePurchase()}>Pay</button>
+            <button className={ buttonClicked ? 'payButton clicked' : 'payButton' } 
+                onClick={() => {
+                    animationOnClick();
+                    setButtonClicked(true);
+                }}>Pay</button>
             </div>
         </div>
     )
