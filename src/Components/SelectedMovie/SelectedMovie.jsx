5c7e72a94cb0053a368988f6ce8feb5f9de70f71
Robert Horngren <robert@horngren.net>
2021-06-09 23:48:51 +0200

    suggestion of displaying reviews

--- src/Components/SelectedMovie/SelectedMovie.jsx
+++ src/Components/SelectedMovie/SelectedMovie.jsx
@@ -15,7 +15,7 @@ const SelectedMovie = () => {
     const [imdbRating, SetImdbRating] = useState([]);
     const [imdbID, SetImdbId] = useState([]);
     let id = movieId;
-    const urlRating ='http://www.omdbapi.com/?i='
+    const urlRating = 'http://www.omdbapi.com/?i='
     const apiKey = '&apikey=fbdcb121'
     const [response, setResponse] = useState([]);
     const [content, setContent] = useState('Loading...');
@@ -23,109 +23,39 @@ const SelectedMovie = () => {
     const [reviewsArray, setReviewsArray] = useState([]);
 
     useEffect(() => {
-        getFlixDetail(dispatch, id).then((resp) => { setFlixDetail(JSON.parse(resp))
+        getFlixDetail(dispatch, id).then((resp) => {
+            setFlixDetail(JSON.parse(resp))
 
-            if(JSON.parse(resp).imdb_id !== null) {
+            if (JSON.parse(resp).imdb_id !== null) {
                 SetImdbId(JSON.parse(resp).imdb_id)
             }
         });
-        
+
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-
-    //Test method 1
 
     useEffect(() => {
         getMovieReviewRating(id).then((resp) => {
             setResponse(JSON.parse(resp))
-        })
-    }, [])
+        });
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
-        if(response.length !== 0 ) {
-            for (let i=0; i < response.reviews.length; i++){
-                console.log(response.reviews[i].comment);
-                console.log(response);
-            }  
-        } 
-    }, [response])
-
-    // function GetUserReview() {
-
-    //     if (response.length !== 0 ) {
-    //         for (let i=0; i < response.reviews.length; i++) {
-    //             return (
-    //                 <div className="user-review">
-    //                     <div className="user-review-text">
-    //                        {response.reviews[i].comment}
-    //                     </div>
-    //                 </div>
-    //             )
-    //         }
-    //     } 
-    // }
-
-    const GetUserReview = () => {
-        try{
-            if (response.length !== 0) {
-                for (let i=0; i < response.reviews.length; i++) {
-                    return (
-                        
-                        <div className="user-review">
-                                <div className="user-review-text">
-                                    {response.reviews[i].comment}
-                                </div>
-                                <div className="user-review-rating"> 
-                                Rating: {response.reviews[i].rate}
-                                </div>
-                        </div>
-                       
-                    )
-                }
-            } else {
-                return (
-                    <div className="user-review">
-                        <div className="user-review-text">
-                            No reviews found
-                        </div>
-                    </div>
-                )
-            }
-        } catch(e) {
-            console.log(e)
+        if (response.length === 0) {
+            setContent('There are no user reviews yet');
+        } else {
+            setReviews(response.reviews);
         }
-    }
+    }, [response]);
 
-
-
-    
-    //test method 2
-    // useEffect(() => {
-    //     getMovieReviewRating(id).then((resp) => {
-    //         setResponse(JSON.parse(resp));
-    //     })
-    // }, []);
-
-    // useEffect(() => {
-    //     if(response !== '') {
-    //         if(response == null) {
-    //             setContent('user not logged in')
-    //         } else if (response.reviews.length === 0) {
-    //             setContent('No reviews found')
-    //         } else {
-    //             setReviews(response.reviews)
-    //         }
-    //     }
-    // }, [response])
-
-    // useEffect(() => {
-    //     if(reviews !== []) {
-    //         setReviewsArray(reviews)
-    //     }
-    // }, [reviews])
+    useEffect(() => {
+        if (reviews.length > 0) {
+            console.log('reviews: ', reviews);
+            setReviewsArray(reviews);
+        }
+    }, [reviews])
 
     function buyMovie() {
-        const movieToBuy = {"movieId": flixDetail.id, "movieTitle": flixDetail.original_title, "price": 4.99};
+        const movieToBuy = { "movieId": flixDetail.id, "movieTitle": flixDetail.original_title, "price": 4.99 };
         dispatch(actions.addItem(movieToBuy));
     }
 
@@ -139,33 +69,26 @@ const SelectedMovie = () => {
     }
 
     async function getIMDBRating() {
-            try {
-                let resp = await fetch(urlRating + imdbID + apiKey);
-                let data = await resp.json();
-                
-                
-                if(imdbID.length) {
-                    if(data.imdbRating !== "N/A"){
-                        if(data.Response !== 'False') {
-                            SetImdbRating(data.imdbRating + "/10")
-                           
-                        }else {
-                            SetImdbRating('No Rating Found')
-                        }
-                        
-                        
-                    } 
-                    
-                } else {
-                    SetImdbRating('No Rating Found')
+        try {
+            let resp = await fetch(urlRating + imdbID + apiKey);
+            let data = await resp.json();
+
+            if (imdbID.length) {
+                if (data.imdbRating !== "N/A") {
+                    if (data.Response !== 'False') {
+                        SetImdbRating(data.imdbRating + "/10")
+                    } else {
+                        SetImdbRating('No Rating Found')
+                    }
                 }
-              
+            } else {
+                SetImdbRating('No Rating Found')
             }
-            catch (error) {
-                console.log(error);
-            }
-}
-        
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 
     useEffect(() => {
         getIMDBRating() // eslint-disable-next-line
@@ -203,30 +126,28 @@ const SelectedMovie = () => {
 
             <div className="user-review-container">
                 <div className="user-review-header">User reviews</div>
-                    {/* method 2*/}
-                    {/* {reviewsArray.length > 0 ?
 
-                        reviewsArray.map((item, index) =>
-                            <div className="user-review" key={index}>
-                                <div className="user-review-text">
-                                    {item.comment}
-                                </div>
-                            </div>
-                        )
-                        :
-                        <div className="user-review">
+                {reviewsArray.length > 0 ?
+
+                    reviewsArray.map((item, index) =>
+
+                        <div className="user-review" key={index}>
                             <div className="user-review-text">
-                                {content}
+                                {item.comment}
+                            </div>
+                            <div className="user-review-rating">
+                                Rating: {item.rate}
                             </div>
                         </div>
-                    } */}
-                
-                {/*test method one */}
-                <GetUserReview />
-                
+                    )
+                    :
+                    <div className="user-review">
+                        <div className="user-review-text">
+                            {content}
+                        </div>
+                    </div>
+                }
             </div>
-
-
         </ScrollContainer>
     )
 }
